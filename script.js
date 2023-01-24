d2fc10e81a28a1cb25f9c5febf40893bb09f1ff7
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-01-24 19:24:43 +0300

    ooops...

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var navs = document.querySelectorAll("nav > a")
 navs.forEach(nav => nav.addEventListener("click", () => load(nav.href.split('#')[1])))
 
 async function load(page) {
-    await fetch("https://raw.githubusercontent.com/xzxADIxzx/Crawler-Language-Vocabulary/main/pages/" + page + ".html").then((response) => {
-        document.getElementById("content").innerText = response.text()
-    })
+    await fetch("https://raw.githubusercontent.com/xzxADIxzx/Crawler-Language-Vocabulary/main/pages/" + page + ".html")
+        .then((response) => response.text())
+        .then((html) => document.getElementById("content").innerHTML = html)
 }
