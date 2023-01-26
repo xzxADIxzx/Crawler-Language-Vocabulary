2321fe64094450ee343b30f36b4a7c585b393d0c
xzxADIxzx <valerazhuk20122006@gmail.com>
2023-01-26 17:47:36 +0300

    add vocabulary

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+var root = "https://raw.githubusercontent.com/xzxADIxzx/Crawler-Language-Vocabulary/main";
+var content = document.getElementById("content");
+
+// #region language
+
+var vocabulary;
+fetch(root + "/language/vocabulary.json").then(response => vocabulary = response.json())
+
+// #endregion
+// #region build
+
 var navs = document.querySelectorAll("nav > a")
 navs.forEach(nav => nav.addEventListener("click", () => load(nav.href.split('#')[1])))
 
 async function load(page) {
-    await fetch("https://raw.githubusercontent.com/xzxADIxzx/Crawler-Language-Vocabulary/main/pages/" + page + ".html")
-        .then((response) => response.text())
-        .then((html) => document.getElementById("content").innerHTML = html)
+    await fetch(root + "/pages/" + page + ".html")
+        .then(response => response.text())
+        .then(html => content.innerHTML = html)
+
+    if (page == "vocabulary") buildVocabulary();
 }
+
+function buildVocabulary() {
+    var list = document.getElementById("word-list")
+    var template = list.innerHTML
+
+    list.innerHTML = ""
+    vocabulary.forEach(word => {
+        list.innerHTML += template.replace("#word", word.word).replace("#translation", word.noun)
+    });
+}
+
+// #endregion
 
 load("about")
